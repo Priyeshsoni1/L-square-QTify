8921f2172af7443bb695743d9ab2dc153159c4f9
Priyesh Soni <priyesh9792@gmail.com>
2025-03-24 20:32:05 +0530

    Added Songs and Faq Sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import HeroSection from "../components/hero-section/HeroSection";
 import Albums from "../components/albums/Albums";
 import Search from "../components/search/Search";
-
+import Songs from "../components/songs/Songs";
+import Faq from "../components/faq/Faq";
 
 const Home = () => {
   return (
     <>
-      <Search placeHolder={"Search a song of your choice"} type={"mobile"}/>
-      <HeroSection/>
-      <Albums/>
-      
+      <Search placeHolder={"Search a song of your choice"} type={"mobile"} />
+      <HeroSection />
+      <Albums />
+      <Songs />
+      {/* <Faq /> */}
     </>
   );
 };
